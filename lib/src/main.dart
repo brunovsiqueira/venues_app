9a7080fe8e5d84e2efd44d05499b7971a5a195ef
Bruno Siqueira <brunovsiq@gmail.com>
2023-07-22 16:32:11 -0300

    add initial configuration for dotEnv; add flutter_riverpod and start building HomePage

---
+++ lib/src/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:venues_app/src/pages/home_page.dart';
+
+Future<void> main() async {
+  await dotenv.load(
+      fileName:
+          "lib/config/env/.${const String.fromEnvironment('env', defaultValue: 'prod')}.env");
+  runApp(const VenuesApp());
+}
+
+class VenuesApp extends StatelessWidget {
+  const VenuesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wolt Venues',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(87, 191, 228, 0.0)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}

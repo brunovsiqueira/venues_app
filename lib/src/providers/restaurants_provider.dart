aa9752c898f212ba24594f5dd3155f3e0a0e99c8
Bruno Siqueira <brunovsiq@gmail.com>
2023-07-31 23:01:52 -0300

    remove unused

--- lib/src/providers/restaurants_provider.dart
+++ lib/src/providers/restaurants_provider.dart
@@ -1,30 +1,13 @@
-import 'dart:async';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:venues_app/src/models/coordinates_model.dart';
 import 'package:venues_app/src/models/restaurants_response_model.dart';
-import 'package:venues_app/src/providers/restaurants_coordinates_provider.dart';
 import 'package:venues_app/src/providers/restaurants_service_provider.dart';
-
-// final restaurantsTimerProvider = Provider<Timer>((ref) {
-//   int locationIndex = 0;
-//   return Timer.periodic(Duration(seconds: 10), (timer) {
-//     final CoordinatesModel currentCoordinate = locations[locationIndex];
-//     // Refresh the venue list provider with the updated location
-//     ref.refresh(
-//         restaurantsProvider(currentCoordinate));
-
-//     locationIndex = (locationIndex + 1) % locations.length;
-//   });
-// });
 
 final restaurantsProvider =
     FutureProvider.family<RestaurantsResponseModel, CoordinatesModel>(
         (ref, coordinates) async {
   print('provider coordinate: ${coordinates.toQueryParams()}');
-  // ref.listen(restaurantCoordinatesProvider, (previous, next) {
-  //   if ()
-  //  });
+
   return ref
       .read(restaurantsServiceProvider)
       .getRestaurants(coordinates: coordinates);

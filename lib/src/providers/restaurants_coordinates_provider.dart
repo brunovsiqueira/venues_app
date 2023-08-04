bca3c0c3cd75000356b240a5167526802f0d5052
Bruno Siqueira <brunovsiq@gmail.com>
2023-08-03 22:12:36 -0300

    pass list by argument to HomePage

--- lib/src/providers/restaurants_coordinates_provider.dart
+++ lib/src/providers/restaurants_coordinates_provider.dart
@@ -4,7 +4,20 @@ import 'package:venues_app/src/viewmodels/restaurant_coordinates_view_model.dart
 
 final restaurantCoordinatesProvider = StateNotifierProvider.autoDispose<
     RestaurantCoordinatesViewModel, CoordinatesModel>((ref) {
-  final restaurantCoordinatesViewModel = RestaurantCoordinatesViewModel();
+  final restaurantCoordinatesViewModel = RestaurantCoordinatesViewModel(
+    [
+      const CoordinatesModel(latitude: 60.170187, longitude: 24.930599),
+      const CoordinatesModel(latitude: 60.169418, longitude: 24.931618),
+      const CoordinatesModel(latitude: 60.169818, longitude: 24.932906),
+      const CoordinatesModel(latitude: 60.170005, longitude: 24.935105),
+      const CoordinatesModel(latitude: 60.169108, longitude: 24.936210),
+      const CoordinatesModel(latitude: 60.168355, longitude: 24.934869),
+      const CoordinatesModel(latitude: 60.167560, longitude: 24.932562),
+      const CoordinatesModel(latitude: 60.168254, longitude: 24.931532),
+      const CoordinatesModel(latitude: 60.169012, longitude: 24.930341),
+      const CoordinatesModel(latitude: 60.170085, longitude: 24.929569),
+    ],
+  );
   restaurantCoordinatesViewModel.startLocationTimer();
   return restaurantCoordinatesViewModel;
   //FIXME: only send 10, doesnt go to the first
--- lib/src/viewmodels/restaurant_coordinates_view_model.dart
+++ lib/src/viewmodels/restaurant_coordinates_view_model.dart
@@ -3,32 +3,21 @@ import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:venues_app/src/models/coordinates_model.dart';
 
-final locations = [
-  const CoordinatesModel(latitude: 60.170187, longitude: 24.930599),
-  const CoordinatesModel(latitude: 60.169418, longitude: 24.931618),
-  const CoordinatesModel(latitude: 60.169818, longitude: 24.932906),
-  const CoordinatesModel(latitude: 60.170005, longitude: 24.935105),
-  const CoordinatesModel(latitude: 60.169108, longitude: 24.936210),
-  const CoordinatesModel(latitude: 60.168355, longitude: 24.934869),
-  const CoordinatesModel(latitude: 60.167560, longitude: 24.932562),
-  const CoordinatesModel(latitude: 60.168254, longitude: 24.931532),
-  const CoordinatesModel(latitude: 60.169012, longitude: 24.930341),
-  const CoordinatesModel(latitude: 60.170085, longitude: 24.929569),
-]; //TODO: PASS list by argument
-
 class RestaurantCoordinatesViewModel extends StateNotifier<CoordinatesModel> {
-  RestaurantCoordinatesViewModel()
+  final List<CoordinatesModel> coordinatesList;
+
+  RestaurantCoordinatesViewModel(this.coordinatesList)
       : super(CoordinatesModel(
-            latitude: locations[0].latitude,
-            longitude: locations[0].longitude));
+            latitude: coordinatesList[0].latitude,
+            longitude: coordinatesList[0].longitude));
 
   int _locationIndex = 0;
   Timer? _timer;
 
   void startLocationTimer() {
     _timer = Timer.periodic(const Duration(seconds: 10), (_) {
-      _locationIndex = (_locationIndex + 1) % locations.length;
-      final currentLocation = locations[_locationIndex];
+      _locationIndex = (_locationIndex + 1) % coordinatesList.length;
+      final currentLocation = coordinatesList[_locationIndex];
       state = CoordinatesModel(
           latitude: currentLocation.latitude,
           longitude: currentLocation.longitude);
